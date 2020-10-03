7440f99f272ff713eb1c5abc01a07c5df11c9a9c
Thomas Pedersen <pedersenthomas@outlook.com>
2020-10-03 09:00:44 +0200

    Test and fix grouper (#2)

---
+++ NonogramNet.Lib.Test/GrouperTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NonogramNet.Lib.Model;
+using Xunit;
+
+namespace NonogramNet.Lib.Test
+{
+    public class GrouperTests
+    {
+        [Fact]
+        public void EmptyBoardHorizontal()
+        {
+            var board = BoardSamples.Board1;
+            var horizontalGroup = Grouper.GroupHorizontal(board, 0);
+            var expected = new List<(CellState state, int count)>
+            {
+                (CellState.None, 5)
+            };
+            Assert.Equal(expected, horizontalGroup);
+        }
+
+        [Fact]
+        public void EmptyBoardVertical()
+        {
+            var board = BoardSamples.Board1;
+            var horizontalGroup = Grouper.GroupVertical(board, 0);
+            var expected = new List<(CellState state, int count)>
+            {
+                (CellState.None, 5)
+            };
+            Assert.Equal(expected, horizontalGroup);
+        }
+
+        [Fact]
+        public void MixedFilledAndEmptyVertical()
+        {
+            var board = BoardSamples.Board1
+                .ApplyChanges(
+                    new BoardChange(0, 0, CellState.Filled),
+                    new BoardChange(0, 1, CellState.Filled),
+                    new BoardChange(0, 3, CellState.Filled)
+                );
+            var verticalGroup = Grouper.GroupVertical(board, 0);
+            var expected = new List<(CellState state, int count)>
+            {
+                (CellState.Filled, 2),
+                (CellState.None, 1),
+                (CellState.Filled, 1),
+                (CellState.None, 1),
+            };
+            Assert.Equal(expected, verticalGroup);
+        }
+    }
+}
